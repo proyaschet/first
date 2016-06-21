fa49ed91eba1514e64dc149c3ed0411a5c19f876
Amarnath <amarnath@amarnaths-macbook-air.local>
2016-06-21 21:14:15 +0530

    Initial Commit

---
+++ boomappTests/boomappTests.swift
@@ -0,0 +1,36 @@
+//
+//  boomappTests.swift
+//  boomappTests
+//
+//  Created by Amarnath on 21/06/16.
+//  Copyright © 2016 Amarnath. All rights reserved.
+//
+
+import XCTest
+@testable import boomapp
+
+class boomappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
